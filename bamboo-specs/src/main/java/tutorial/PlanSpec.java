ceeef9ebaf064c05c0345b44798361eb4612acef
MaximumQuasar <shaikasiefhussain@gmail.com>
2020-11-06 18:59:49 +0530

    Update PlanSpec.java

--- bamboo-specs/src/main/java/tutorial/PlanSpec.java
+++ bamboo-specs/src/main/java/tutorial/PlanSpec.java
@@ -1,6 +1,7 @@
 import com.atlassian.bamboo.specs.api.BambooSpec;
 import com.atlassian.bamboo.specs.api.builders.BambooKey;
 import com.atlassian.bamboo.specs.api.builders.BambooOid;
+import com.atlassian.bamboo.specs.api.builders.notification.Notification;
 import com.atlassian.bamboo.specs.api.builders.permission.PermissionType;
 import com.atlassian.bamboo.specs.api.builders.permission.Permissions;
 import com.atlassian.bamboo.specs.api.builders.permission.PlanPermissions;
@@ -12,32 +13,71 @@ import com.atlassian.bamboo.specs.api.builders.plan.branches.BranchCleanup;
 import com.atlassian.bamboo.specs.api.builders.plan.branches.PlanBranchManagement;
 import com.atlassian.bamboo.specs.api.builders.plan.configuration.ConcurrentBuilds;
 import com.atlassian.bamboo.specs.api.builders.project.Project;
+import com.atlassian.bamboo.specs.builders.notification.PlanFailedNotification;
+import com.atlassian.bamboo.specs.builders.notification.ResponsibleRecipient;
+import com.atlassian.bamboo.specs.builders.notification.WatchersRecipient;
+import com.atlassian.bamboo.specs.builders.task.CheckoutItem;
 import com.atlassian.bamboo.specs.builders.task.ScriptTask;
+import com.atlassian.bamboo.specs.builders.task.VcsCheckoutTask;
+import com.atlassian.bamboo.specs.builders.trigger.RepositoryPollingTrigger;
 import com.atlassian.bamboo.specs.model.task.ScriptTaskProperties;
 import com.atlassian.bamboo.specs.util.BambooServer;
 
 @BambooSpec
 public class PlanSpec {
     
-    public Plan createPlan() {
+    public Plan plan() {
         final Plan plan = new Plan(new Project()
-                .oid(new BambooOid("o8et7z0jo26a"))
-                .key(new BambooKey("PRJ"))
-                .name("Project Name"),
-            "Plan Name",
-            new BambooKey("PLANKEY"))
-            .oid(new BambooOid("o8540dnbu9s4"))
-            .description("Plan created from (enter repository url of your plan)")
+                .oid(new BambooOid("o8et7z0jo1dt"))
+                .key(new BambooKey("AC"))
+                .name("Accounting")
+                .description(".NET project accounting"),
+            "Dragon Slayer Quest",
+            new BambooKey("SLAYER"))
+            .oid(new BambooOid("o8540dnbu9s3"))
             .pluginConfigurations(new ConcurrentBuilds())
             .stages(new Stage("Stage 1")
-                    .jobs(new Job("Build and Run",
-                            new BambooKey("RUN"))
-                            .tasks(new ScriptTask()
+                    .jobs(new Job("Job 1",
+                            new BambooKey("JOB1"))
+                            .tasks(new VcsCheckoutTask()
+                                    .checkoutItems(new CheckoutItem().defaultRepository()),
+                                new ScriptTask()
                                     .interpreter(ScriptTaskProperties.Interpreter.BINSH_OR_CMDEXE)
-                                    .inlineBody("echo ${bamboo.planKey}\n\n${bamboo.capability.system.builder.mvn3.mvn} --version"))))
+                                    .inlineBody("${bamboo_capability_system_builder_mvn3_Maven_3}/bin/mvn clean test"))))
+            .linkedRepositories("Accounting")
+            
+            .triggers(new RepositoryPollingTrigger())
             .planBranchManagement(new PlanBranchManagement()
-                    .delete(new BranchCleanup()))
+                    .createForVcsBranch()
+                    .delete(new BranchCleanup()
+                        .whenRemovedFromRepositoryAfterDays(1)
+                        .whenInactiveInRepositoryAfterDays(30))
+                    .notificationLikeParentPlan())
+            .notifications(new Notification()
+                    .type(new PlanFailedNotification())
+                    .recipients(new ResponsibleRecipient(),
+                        new WatchersRecipient()))
             .forceStopHungBuilds();
         return plan;
+    }
+    
+    public PlanPermissions planPermission() {
+        final PlanPermissions planPermission = new PlanPermissions(new PlanIdentifier("AC", "SLAYER"))
+            .permissions(new Permissions()
+                    .loggedInUserPermissions(PermissionType.VIEW)
+                    .anonymousUserPermissionView());
+        return planPermission;
+    }
+    
+    public static void main(String... argv) {
+        //By default credentials are read from the '.credentials' file.
+        BambooServer bambooServer = new BambooServer("http://192.168.31.63:8085");
+        final PlanSpec planSpec = new PlanSpec();
+        
+        final Plan plan = planSpec.plan();
+        bambooServer.publish(plan);
+        
+        final PlanPermissions planPermission = planSpec.planPermission();
+        bambooServer.publish(planPermission);
     }
 }
